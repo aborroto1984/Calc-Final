258d64cd71f109ef0c281782ce41cd52a5f7a740
Alfredo Borroto <aborroto1984@hotmail.com>
2022-10-22 16:57:22 -0400

    Initial Commit

---
+++ Calc+ Final/Calc__FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calc__FinalApp.swift
+//  Calc+ Final
+//
+//  Created by Alfredo Borroto on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Calc__FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
