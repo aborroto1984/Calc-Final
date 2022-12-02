5d8b3f937e4bc5b5895552e32459dadf9f6a8336
Alfredo Borroto <aborroto1984@hotmail.com>
2022-12-02 16:05:22 -0500

    Created two enums with the numerators and denominators, created a number formatter to recognize what number to type when the numbers are taped

--- Calc+ Final/Views/Calculator view.swift
+++ Calc+ Final/Views/Calculator view.swift
@@ -6,6 +6,30 @@
 //
 
 import SwiftUI
+enum numerator: String{
+    case upZero  = "⁰"
+    case upOne   = "¹"
+    case upTwo   = "²"
+    case upThree = "³"
+    case upFour  = "⁴"
+    case upFive  = "⁵"
+    case upSix   = "⁶"
+    case upSeven = "⁷"
+    case upEight = "⁸"
+    case upNine  = "⁹"
+}
+enum denominator: String{
+    case downZero  = "₀"
+    case downOne   = "₁"
+    case downTwo   = "₂"
+    case downThree = "₃"
+    case downFour  = "₄"
+    case downFive  = "₅"
+    case downSix   = "₆"
+    case downSeven = "₇"
+    case downEight = "₈"
+    case downNine  = "₉"
+}
 
 struct Calculator_view: View {
     
@@ -13,10 +37,11 @@ struct Calculator_view: View {
     @State var upValue = "0"
     @State var value = "0"
     @State var currOpp = ""
-    @State var numLock = false
-    
+    @State var typingNumerator = false
+    @State var typingDenominator = false
     let calc = Calculator()
     let opps = ["+","−","×","÷"]
+    
     
     var body: some View {
         
@@ -33,9 +58,6 @@ struct Calculator_view: View {
                 Text("Calculator")
                 
                     .font(.largeTitle).foregroundColor(.black)
-                
-                
-                
             }
             
             VStack{
@@ -49,30 +71,33 @@ struct Calculator_view: View {
                         .bold()
                         .font(.system(size: 70))
                              , alignment: .bottomTrailing)
-            }
-            
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.7)
+                    .padding()
+                
+            }.scaledToFill()
             
             HStack{
                 
                 Button(action: {
                     
-                }, label: {Image("feetButton").resizable().scaledToFit()})
+                }, label: {Image("feetButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {
                     
-                }, label: {Image("inchButton").resizable().scaledToFit()})
+                }, label: {Image("inchButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {
                     
-                }, label: {Image("yardButton").resizable().scaledToFit()})
+                }, label: {Image("yardButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {
                     
-                }, label: {Image("copyButton").resizable().scaledToFit()})
+                }, label: {Image("copyButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {
                     
-                }, label: {Image("pasteButton").resizable().scaledToFit()})
+                }, label: {Image("pasteButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
             }
             
             HStack{
@@ -82,94 +107,94 @@ struct Calculator_view: View {
                 
                 Button(action: {
                     
-                }, label: {Image("plusMinusButton").resizable().scaledToFit()})
+                }, label: {Image("plusMinusButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {
                     
-                }, label: {Image("percentButton").resizable().scaledToFit()})
+                }, label: {Image("percentButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {transferValue(opp: "÷")
-                }, label: {Image("divideButton").resizable().scaledToFit()})
+                }, label: {Image("divideButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {
                     
-                }, label: {Image("xOver2Button").resizable().scaledToFit()})
+                }, label: {Image("xOver2Button").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
             }
             
             HStack{
-                Button(action: {buttonTaped(number: "7")
+                Button(action: {buttonTaped(number: numberFormater(number: "7"))
                     
                 }, label: {Image("sevenButton").resizable().scaledToFit()})
                 
-                Button(action: {buttonTaped(number: "8")
+                Button(action: {buttonTaped(number: numberFormater(number: "8"))
                     
                 }, label: {Image("eightButton").resizable().scaledToFit()})
                 
-                Button(action: {buttonTaped(number: "9")
+                Button(action: {buttonTaped(number: numberFormater(number: "9"))
                     
                 }, label: {Image("nineButton").resizable().scaledToFit()})
                 
                 Button(action: {transferValue(opp: "×")
                     
-                }, label: {Image("multiplyButton").resizable().scaledToFit()})
+                }, label: {Image("multiplyButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {
                     
-                }, label: {Image("xOver4Button").resizable().scaledToFit()})
+                }, label: {Image("xOver4Button").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
             }
             
             HStack{
-                Button(action: {buttonTaped(number: "4")
+                Button(action: {buttonTaped(number: numberFormater(number: "4"))
                     
                 }, label: {Image("fourButton").resizable().scaledToFit()})
                 
-                Button(action: {buttonTaped(number: "5")
+                Button(action: {buttonTaped(number: numberFormater(number: "5"))
                     
                 }, label: {Image("fiveButton").resizable().scaledToFit()})
                 
-                Button(action: {buttonTaped(number: "6")
+                Button(action: {buttonTaped(number: numberFormater(number: "6"))
                     
                 }, label: {Image("sixButton").resizable().scaledToFit()})
                 
                 Button(action: {transferValue(opp: "−")
                     
-                }, label: {Image("minusButton").resizable().scaledToFit()})
+                }, label: {Image("minusButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {
                     
-                }, label: {Image("xOver8Button").resizable().scaledToFit()})
+                }, label: {Image("xOver8Button").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
             }
             
             HStack{
-                Button(action: {buttonTaped(number: "1")
+                Button(action: {buttonTaped(number: numberFormater(number: "1"))
                     
                 }, label: {Image("oneButton").resizable().scaledToFit()})
                 
-                Button(action: {buttonTaped(number: "2")
+                Button(action: {buttonTaped(number: numberFormater(number: "2"))
                     
                 }, label: {Image("twoButton").resizable().scaledToFit()})
                 
-                Button(action: {buttonTaped(number: "3")
+                Button(action: {buttonTaped(number: numberFormater(number: "3"))
                     
                 }, label: {Image("threeButton").resizable().scaledToFit()})
                 
                 Button(action: {transferValue(opp: "+")
                     
-                }, label: {Image("plusButton").resizable().scaledToFit()})
+                }, label: {Image("plusButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {
                     
-                }, label: {Image("xOver16Button").resizable().scaledToFit()})
+                }, label: {Image("xOver16Button").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
             }
             
             HStack{
-                Button(action: {buttonTaped(number: "0")
+                Button(action: {buttonTaped(number: numberFormater(number: "0"))
                     
                 }, label: {Image("zeroButton").resizable().scaledToFit()})
                 
                 Button(action: {buttonTaped(number: ".")
                     
-                }, label: {Image("pointButton").resizable().scaledToFit()})
+                }, label: {Image("pointButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
                 Button(action: {back()
                     
@@ -177,14 +202,71 @@ struct Calculator_view: View {
                 
                 Button(action: {equal()
                     
-                }, label: {Image("equalButton").resizable().scaledToFit()})
+                }, label: {Image("equalButton").resizable().scaledToFit()}).opacity(typingNumerator || typingDenominator ? 0 : 1)
                 
-                Button(action: {
+                Button(action: {buttonTaped(number: "⁄")
                     
                 }, label: {Image("emptyFractionButton").resizable().scaledToFit()})
                 
             }
         }.onAppear{UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue, forKey: "orientation"); AppDelegate.orientationLock = .portrait}.padding()
+    }
+    
+    func numberFormater(number: String)-> String{
+        switch number{
+        case "0":
+            if self.typingNumerator{return numerator.upZero.rawValue}
+            else if self.typingDenominator{return denominator.downZero.rawValue}
+            else{return "0"}
+            
+        case "1":
+            if self.typingNumerator{return numerator.upOne.rawValue}
+            else if self.typingDenominator{return denominator.downOne.rawValue}
+            else{return "1"}
+            
+        case "2":
+            if self.typingNumerator{return numerator.upTwo.rawValue}
+            else if self.typingDenominator{return denominator.downTwo.rawValue}
+            else{return "2"}
+            
+        case "3":
+            if self.typingNumerator{return numerator.upThree.rawValue}
+            else if self.typingDenominator{return denominator.downThree.rawValue}
+            else{return "3"}
+            
+        case "4":
+            if self.typingNumerator{return numerator.upFour.rawValue}
+            else if self.typingDenominator{return denominator.downFour.rawValue}
+            else{return "4"}
+            
+        case "5":
+            if self.typingNumerator{return numerator.upFive.rawValue}
+            else if self.typingDenominator{return denominator.downFive.rawValue}
+            else{return "5"}
+            
+        case "6":
+            if self.typingNumerator{return numerator.upSix.rawValue}
+            else if self.typingDenominator{return denominator.downSix.rawValue}
+            else{return "6"}
+            
+        case "7":
+            if self.typingNumerator{return numerator.upSeven.rawValue}
+            else if self.typingDenominator{return denominator.downSeven.rawValue}
+            else{return "7"}
+            
+        case "8":
+            if self.typingNumerator{return numerator.upEight.rawValue}
+            else if self.typingDenominator{return denominator.downEight.rawValue}
+            else{return "8"}
+            
+        case "9":
+            if self.typingNumerator{return numerator.upNine.rawValue}
+            else if self.typingDenominator{return denominator.downNine.rawValue}
+            else{return "9"}
+            
+        default:
+            return ""
+        }
     }
     
     func clear(){
@@ -220,7 +302,7 @@ struct Calculator_view: View {
         //self.numLock = false
         // Safe guards against multiple opperators
         if self.upValue.contains("+") || self.upValue.contains("−") || self.upValue.contains("×") || self.upValue.contains("÷") {equal()}
-        
+
         if self.upValue == "0"{
             self.upValue = "\(self.value)\(opp)"
             calc.num1 = Float(self.value) ?? 0
@@ -296,13 +378,14 @@ struct Calculator_view: View {
         }
     }
     func buttonTaped(number: String){
-            if self.value.count < 8 {
+            if self.value.count < 11 {
+
                 // Safe guards against decimal point duplicates
                 if self.value.contains(".") && number == "." {return}
                 else if self.currOpp.isEmpty && self.upValue != "0"{
                     self.upValue = "0"
                 }
-                
+
                 if self.value == "0" && number == "." {
                     self.value = "\(self.value)\(number)"
                 }
@@ -315,6 +398,11 @@ struct Calculator_view: View {
                 
                 if self.upValue != "0" {
                     self.upValue = "\(self.upValue)\(number)"
+                }
+                
+                // Typing a fraction
+                if self.value == "⁄"{
+                    self.typingNumerator = true
                 }
             }
         }
